5e0ab9de1a0d9624d1332e4f2dbb066d5205b590
Akshay Meher <akshay_111meher@hotmail.com>
2024-01-25 21:05:55 +0400

    update

--- llama/build.sh
+++ llama/build.sh
@@ -7,4 +7,4 @@ fi
 docker rmi -f $(docker images -a -q)
 docker build --no-cache ./ -t nitroimg
 nitro-cli build-enclave --docker-uri nitroimg:latest --output-file nitro-enclave.eif
-nitro-cli run-enclave --cpu-count 2 --memory 5000 --eif-path nitro-enclave.eif --enclave-cid 88
+nitro-cli run-enclave --cpu-count 4 --memory 5000 --eif-path nitro-enclave.eif --enclave-cid 88
