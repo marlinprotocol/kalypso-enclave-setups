a8d6876b4fe7e3513f18dc60d820d7ca54899957
Akshay Meher <akshay_111meher@hotmail.com>
2024-01-25 21:59:23 +0400

    update

--- llama/setup.sh
+++ llama/setup.sh
@@ -17,9 +17,7 @@ iptables -L -t nat
 /app/keygen --secret /app/id.sec --public /app/id.pub
 /app/oyster-keygen --secretpath /app/secp.sec
 
-# /app/ollama pull llama2
-
-ollama pull llama2
+/app/ollama pull llama2
 
 # starting supervisord
 /app/supervisord
