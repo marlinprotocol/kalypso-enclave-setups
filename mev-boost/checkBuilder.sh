f7104ab5b76780218de6cc1119448a82c1fdaecb
Akshay Meher <akshay_111meher@hotmail.com>
2024-03-05 14:40:41 +0400

    update

--- mev-boost/checkBuilder.sh
+++ mev-boost/checkBuilder.sh
@@ -4,7 +4,7 @@ URL="curl http://localhost:9062/eth/v1/builder/status"
 
 # Infinite loop to run curl every 10 seconds
 while true; do
-  curl $URL
+  curl $URL 2>/dev/null
   echo "Sleeping for 10 seconds..."
   sleep 10
 done
