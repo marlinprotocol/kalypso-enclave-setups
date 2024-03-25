16cbedee322225eee9d56b52809e3327eb41c50c
Akshay Meher <akshay_111meher@hotmail.com>
2024-03-25 14:39:35 +0400

    update

--- snarkvm/snark.sh
+++ snarkvm/snark.sh
@@ -1,5 +1,5 @@
-./snarkvm build
-./snarkvm run hello 2u32 3u32
+# ./snarkvm build
+# ./snarkvm run hello 2u32 3u32
 ./snarkvm execute hello 2u32 3u32
 
 echo "Snark Testing Inside Enclave Completed"
