57c2c04cf0430610eb0f88d74f613f8b19a3b42d
Akshay Meher <akshay_111meher@hotmail.com>
2024-05-01 12:21:10 +0400

    update ip table configs

--- amd64-modular-generator/setup.sh
+++ amd64-modular-generator/setup.sh
@@ -5,12 +5,11 @@ ifconfig lo 127.0.0.1
 ifconfig
 
 # adding a default route
-ip route add default via 127.0.0.1 dev lo
+ip route add default dev lo src 127.0.0.1
 route -n
 
 # iptables rules to route traffic to transparent proxy
 iptables -A OUTPUT -t nat -p tcp --dport 1:65535 ! -d 127.0.0.1  -j DNAT --to-destination 127.0.0.1:1200
-iptables -t nat -A POSTROUTING -o lo -s 0.0.0.0 -j SNAT --to-source 127.0.0.1
 iptables -L -t nat
 
 # generate identity key
